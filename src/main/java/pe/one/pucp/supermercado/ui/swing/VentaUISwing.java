92fe0811d994a9caf03da48cd868604054264955
lmiguelmh <lmiguelmh@gmail.com>
2014-10-28 23:29:50 -0500

    Starting with venta and venta_detalle
    First try with UI

---
+++ src/main/java/pe/one/pucp/supermercado/ui/swing/VentaUISwing.java
@@ -0,0 +1,13 @@
+package pe.one.pucp.supermercado.ui.swing;
+
+import javax.swing.*;
+
+public class VentaUISwing {
+    private JPanel mainPanel;
+    private JTable table1;
+    private JTable table2;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton buscarVendedorButton;
+    private JButton buscarClienteButton;
+}
